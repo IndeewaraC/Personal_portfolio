0b18851c49fc39cfec720ccfd24480890e1cd0a7
IndeewaraC <indeewaragunathilaka@gmail.com>
2026-03-11 18:06:19 -0500

    Add Cypress Test Stuite

---
+++ cypress/e2e/portfolio_ML.cy.js
@@ -0,0 +1,6 @@
+describe('Muditha Lakmali - Portfolio', () => {
+  //The test cases are following in this suite runs with staging environment created in vercel.
+  //Version control is with GIithub and useing Cypress for testing.
+
+
+});
